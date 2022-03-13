44e28595e854e8874a4ee7906ab2e203753ec716
Naresh <pnareshsatya@gmail.com>
2022-03-13 10:47:04 +0530

    added project decription

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { DataServiceService } from 'src/app/service/dataservice/data-service.service';
 
 @Component({
@@ -8,8 +10,14 @@ import { DataServiceService } from 'src/app/service/dataservice/data-service.ser
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private dataService:DataServiceService) { }
+  constructor(private dataService:DataServiceService, private router:Router, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {  }
 
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigateByUrl("/login");
+    console.log("Logout successfull");
+    this.snackBar.open("successfully logged out", "dismiss", {duration:3000});
+  }
 }
